ba06f672bad2a5d077621642d536e66139077d69
mikaelovi <mikaelovi@gmail.com>
2019-10-15 18:21:53 +0100

    enhanced the users and search components

--- src/components/Users/Search.js
+++ src/components/Users/Search.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
 
 export class Search extends Component {
   state = {
     text: ''
   };
 
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+  }
+
   onChange = (e) => this.setState({[e.target.name ]: e.target.value});
 
   onSubmit = e => {
     e.preventDefault();
-    console.log(this.state.text);
+    this.props.searchUsers(this.state.text);
+    this.setState({text: ''});
   }
   
    
   render() {
+    const {showClear, clearUsers} = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit.bind(this)} className="form">
           <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.onChange}/>
           <input type="submit" value="Search" className="btn btn-dark block" />
         </form>
+        {showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}
+        
       </div>
     )
   }
